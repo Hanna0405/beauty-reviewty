b2c3e5c4b233eb659e8337d14fb75a4c9626c762
Hanna0405 <a.katrusha@gmail.com>
2025-08-17 15:58:17 -0700

    fix: correct storageBucket

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA0BcaNagg0p6DjJdNrrKfyQIUToq96Xw',         
-  authDomain: 'beauty-reviewty.firebaseapp.com',             
-  projectId: 'beauty-reviewty',                              
-  storageBucket: 'beauty-reviewty.firebasestorage.app',      
-  messagingSenderId: '506141876880',                         
-  appId: '1:506141876880:web:22c2abffc42fa133eaeE15',        
-  measurementId: 'G-MTZFM6ZXRK',                             
+  apiKey: "AIzaSyA0CchaNagg0pG6jjdNnrxfYqUIFOq96Xw",         
+  authDomain: "beauty-reviewty.firebaseapp.com",             
+  projectId: "beauty-reviewty",                              
+  storageBucket: "beauty-reviewty.appspot.com",      
+  messagingSenderId: "506141876880",                         
+  appId: "1:506141876880:web:22c2abffc42fa133eaee15",        
+  measurementId: "G-MT2FM6ZXRF",                             
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
@@ -20,3 +20,4 @@ export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
